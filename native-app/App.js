2618e64a22423e93ddd6203754391668bb43caa1
bakorx <ekbaddai@st.ug.edu.gh>
2024-05-26 12:35:12 +0000

    made the name bold

--- native-app/App.js
+++ native-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is KwameBakor</Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldText}>KwameBakor</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,4 +21,8 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
   },
+
+  boldText: {
+    fontWeight: 'bold',
+  }
 });
