4627d866c28af8fe5be42ec72fc58ce4af5f0020
bakorx <ekbaddai@st.ug.edu.gh>
2024-05-26 12:25:06 +0000

    changed font size to 24

--- native-app/App.js
+++ native-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is KwameBakor</Text>
+      <Text style={styles.text}>My name is KwameBakor</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#0000ff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 24,
   },
 });
